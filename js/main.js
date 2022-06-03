0bcc4164c24a9b4b1c0bd4311935661c23954f23
muhammadBobur0 <muhammadboburbek@gmail.com>
2022-06-03 20:25:26 +0500

    add new code

--- js/main.js
+++ js/main.js
@@ -3,16 +3,33 @@ var elButton = document.querySelector(".button");
 var elBoxContent = document.querySelector("body");
 
 
+
 elButton.addEventListener("click", function(){
   elBoxContent.classList.toggle("black")
 });
 
 
 
-var eltext = document.querySelector(".button");
-var elBoxContent = document.querySelector("Sign-In", "menu__link");
+var lButton = document.querySelector(".button-header")
+var lBoxContent = document.querySelector(".menu__link")
+var eltext = document.querySelector(".link")
+var elwhite = document.querySelector(".link-Pricing")
+var elResources = document.querySelector(".link-Resources")
+var ellogo = document.querySelector(".logo")
+var ellogobg = document.querySelector(".logo-bg")
+var elSign = document.querySelector(".Sign-In")
 
 
-elButton.addEventListener("click", function(){
-  elBoxContent.classList.toggle("text")
+lButton.addEventListener("click", function(){
+  lBoxContent.classList.toggle("text")
+  eltext.classList.toggle("text")
+  elwhite.classList.toggle("text")
+  elResources.classList.toggle("text")
+  ellogo.classList.toggle("logo-bg")
+  ellogobg.classList.toggle("logo")
+  ellogobg.classList.toggle("logo-bg")
+  elSign.classList.toggle("in")
+
+
+
 });
