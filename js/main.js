ad8d64ccaf293f3a3e16af42ef784898f412dac4
muhammadBobur0 <muhammadboburbek@gmail.com>
2022-06-03 19:13:08 +0500

    add new product

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+var elButton = document.querySelector(".button");
+var elBoxContent = document.querySelector("body");
+
+
+elButton.addEventListener("click", function(){
+  elBoxContent.classList.toggle("black")
+});
+
+
+
+var eltext = document.querySelector(".button");
+var elBoxContent = document.querySelector("Sign-In", "menu__link");
+
+
+elButton.addEventListener("click", function(){
+  elBoxContent.classList.toggle("text")
+});
